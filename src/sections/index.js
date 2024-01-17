e1d16531cc72108804a6d4724c212c33b1ae3871
GOKUL R <rajugokul003@gmail.com>
2024-01-17 21:13:31 +0530

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProduct from './PopularProduct';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import CustomerReview from './CustomerReview';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopularProduct,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    CustomerReview,
+    Footer,
+}
